8b5361a9bc9829d09193f4fdb061c69bc57447a3
CyrusJavan <javan.cyrus@gmail.com>
2020-01-16 22:32:20 -0800

    Add benchmarking and rename

---
+++ src/keyvaluestore/basicmem.go
@@ -0,0 +1,21 @@
+package keyvaluestore
+
+import "fmt"
+
+// BasicMemory is the simplest possible in memory database
+// utilizing just a regular map to store keys and values.
+type BasicMemory map[string]string
+
+// Get the value of the key
+func (s BasicMemory) Get(key string) (string, error) {
+	if val, ok := s[key]; ok {
+		return val, nil
+	}
+	return "", fmt.Errorf("Key not found")
+}
+
+// Set the key to the value
+func (s BasicMemory) Set(key, value string) error {
+	s[key] = value
+	return nil
+}
