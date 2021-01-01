9172925088c28cb15c9d7aa5bc2acd7c0ab2cc19
Cyrus Javan <javan.cyrus@gmail.com>
2021-01-01 13:34:13 -0800

    implement raft.FSM

--- cmd/doviiserver/main.go
+++ cmd/doviiserver/main.go
@@ -3,86 +3,90 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
-	"sync"
+	"strconv"
 	"time"
 
 	"github.com/CyrusJavan/dovii"
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/raft"
 	"github.com/hashicorp/raft-boltdb"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type server struct {
-	store  *dovii.KVStore
-	router *mux.Router
+	store      *dovii.KVStore
+	router     *mux.Router
+	raftServer *raft.Raft
+}
+
+type ServerConfig struct {
+	ID string
 }
 
 func main() {
+	var sc ServerConfig
+	err := envconfig.Process("dovii", &sc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	store, err := dovii.NewKVStore(dovii.BitcaskEngine)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	r, err := newRaftServer(&sc, store)
+	if err != nil {
+		log.Println("raft:", err)
+	}
+
 	s := &server{
-		store:  store,
-		router: mux.NewRouter(),
+		store:      store,
+		router:     mux.NewRouter(),
+		raftServer: r,
 	}
 
 	s.routes()
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		err := http.ListenAndServe("0.0.0.0:7070", s)
-		if err != nil {
-			log.Println("listenandserve:", err)
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		err := doRaft()
-		if err != nil {
-			log.Println("raft:", err)
-		}
-		wg.Done()
-	}()
-
-	wg.Wait()
+	err = http.ListenAndServe("0.0.0.0:7070", s)
+	if err != nil {
+		log.Println("listenandserve:", err)
+	}
 }
 
-func doRaft() error {
+func newRaftServer(sc *ServerConfig, store *dovii.KVStore) (*raft.Raft, error) {
+	log.Println("unique node ID:", sc.ID)
 	conf := raft.DefaultConfig()
-	bs, err := raftboltdb.NewBoltStore("/tmp/raftboltdbstore")
+	conf.LocalID = raft.ServerID(sc.ID)
+	bs, err := raftboltdb.NewBoltStore(fmt.Sprintf("/tmp/raftboltdbstore-%s", sc.ID))
 	if err != nil {
-		return fmt.Errorf("init boltstore: %w", err)
+		return nil, fmt.Errorf("init boltstore: %w", err)
 	}
 
-	ss := &raft.DiscardSnapshotStore{}
+	ss := raft.DiscardSnapshotStore{}
 
-	t, err := raft.NewTCPTransport("localhost:7777", nil, 3, time.Second, os.Stdout)
+	adAddr := net.TCPAddr{
+		IP:   []byte("10.0.0.2"),
+		Port: 7777,
+	}
+	log.Println("advertise address:", string(adAddr.IP)+":"+strconv.Itoa(adAddr.Port))
+	t, err := raft.NewTCPTransport("0.0.0.0:7777", &adAddr, 3, time.Second, os.Stdout)
 	if err != nil {
-		return fmt.Errorf("new tcp transport: %w", err)
+		return nil, fmt.Errorf("new tcp transport: %w", err)
 	}
 
-	servConf := raft.Configuration{Servers: []raft.Server{
-		{
-			Suffrage: raft.Voter,
-			ID:       "node1",
-			Address:  "0.0.0.0:7777",
-		},
-	}}
-
-	err = raft.BootstrapCluster(conf, bs, bs, ss, t, servConf)
+	fsm := BitcaskFSM{}
+	raftServer, err := raft.NewRaft(conf, &fsm, bs, bs, &ss, t)
 	if err != nil {
-		return fmt.Errorf("bootstrapping cluster: %w", err)
+		return nil, fmt.Errorf("starting raft rpc server: %w", err)
 	}
 
-	return nil
+	return raftServer, nil
 }
 
 func (s *server) routes() {
@@ -127,6 +131,67 @@ func (s *server) getHandler() http.HandlerFunc {
 		}
 	}
 }
+
+type LogMessage struct {
+	Key   string
+	Value string
+}
+
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
+}
+
+type BitcaskFSM struct {
+	store *dovii.KVStore
+}
+
+func (fsm *BitcaskFSM) Apply(raftLog *raft.Log) interface{} {
+	if raftLog.Type != raft.LogCommand {
+		log.Println("raftLog.Type is not LogCommand")
+		return nil
+	}
+	var lm LogMessage
+	err := json.Unmarshal(raftLog.Data, &lm)
+	if err != nil {
+		log.Println("decode raftLog.Data:", err)
+		return err
+	}
+	err = fsm.store.Set(lm.Key, lm.Value)
+	if err != nil {
+		log.Println("setting value to store:", err)
+		return err
+	}
+	return nil
+}
+
+func (fsm *BitcaskFSM) Snapshot() (raft.FSMSnapshot, error) {
+	return newSnapshotNoop()
+}
+
+func (fsm *BitcaskFSM) Restore(rc io.ReadCloser) error {
+	decoder := json.NewDecoder(rc)
+	for decoder.More() {
+		var lm LogMessage
+		err := decoder.Decode(&lm)
+		if err != nil {
+			log.Println("restore decode message:", err)
+			return err
+		}
+		err = fsm.store.Set(lm.Key, lm.Value)
+		if err != nil {
+			log.Println("restore setting value to store:", err)
+			return err
+		}
+	}
+	return nil
+}
+
+type snapshotNoop struct{}
+
+func (s snapshotNoop) Persist(_ raft.SnapshotSink) error { return nil }
+
+func (s snapshotNoop) Release() {}
+
+func newSnapshotNoop() (raft.FSMSnapshot, error) {
+	return &snapshotNoop{}, nil
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/hashicorp/go-uuid v1.0.1
 	github.com/hashicorp/raft v1.2.0
 	github.com/hashicorp/raft-boltdb v0.0.0-20171010151810-6e5ba93211ea
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/prologic/bitcask v0.3.10
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/exp v0.0.0-20201229011636-eab1b5eb1a03 // indirect
