ecdf2d36c41764cbc3a7f84c4157c0f7d046ab5a
Cyrus Javan <javan.cyrus@gmail.com>
2021-01-01 16:00:30 -0800

    fix cluster

--- cmd/doviiserver/main.go
+++ cmd/doviiserver/main.go
@@ -25,10 +25,10 @@ type server struct {
 }
 
 type ServerConfig struct {
-	ID string
 }
 
 func main() {
+	log.SetOutput(os.Stderr)
 	var sc ServerConfig
 	err := envconfig.Process("dovii", &sc)
 	if err != nil {
@@ -60,30 +60,57 @@ func main() {
 }
 
 func newRaftServer(sc *ServerConfig, store *dovii.KVStore) (*raft.Raft, error) {
-	log.Println("unique node ID:", sc.ID)
+	adAddr := GetOutboundTCPAddr()
+	adAddr.Port = 7654
 	conf := raft.DefaultConfig()
-	conf.LocalID = raft.ServerID(sc.ID)
-	bs, err := raftboltdb.NewBoltStore(fmt.Sprintf("/tmp/raftboltdbstore-%s", sc.ID))
+	conf.LocalID = raft.ServerID(adAddr.String())
+	bs, err := raftboltdb.NewBoltStore(fmt.Sprintf("/tmp/raftboltdbstore-%s", adAddr.String()))
 	if err != nil {
 		return nil, fmt.Errorf("init boltstore: %w", err)
 	}
 
 	ss := raft.DiscardSnapshotStore{}
 
-	adAddr := net.TCPAddr{
-		IP:   []byte("10.0.0.2"),
-		Port: 7777,
-	}
-	log.Println("advertise address:", string(adAddr.IP)+":"+strconv.Itoa(adAddr.Port))
-	t, err := raft.NewTCPTransport("0.0.0.0:7777", &adAddr, 3, time.Second, os.Stdout)
+	log.Println("advertise address:", adAddr.String())
+	t, err := raft.NewTCPTransport("0.0.0.0:"+strconv.Itoa(adAddr.Port), &adAddr, 3, time.Second, log.Writer())
 	if err != nil {
 		return nil, fmt.Errorf("new tcp transport: %w", err)
 	}
 
-	fsm := BitcaskFSM{}
+	fsm := BitcaskFSM{
+		store: store,
+	}
 	raftServer, err := raft.NewRaft(conf, &fsm, bs, bs, &ss, t)
 	if err != nil {
 		return nil, fmt.Errorf("starting raft rpc server: %w", err)
+	}
+	hn, err := os.Hostname()
+	if err != nil {
+		log.Println("getting hostname:", err)
+	}
+	log.Println(hn)
+
+	addrs, err := net.LookupHost("dovii")
+	if err != nil {
+		return nil, fmt.Errorf("dns lookup: %w", err)
+	}
+
+	log.Println("addrs:", addrs)
+
+	var servers []raft.Server
+	for _, addr := range addrs {
+		servers = append(servers, raft.Server{
+			Suffrage: raft.Voter,
+			ID:       raft.ServerID(addr + ":" + strconv.Itoa(adAddr.Port)),
+			Address:  raft.ServerAddress(addr + ":" + strconv.Itoa(adAddr.Port)),
+		})
+	}
+
+	nodeConfig := raft.Configuration{Servers: servers}
+	future := raftServer.BootstrapCluster(nodeConfig)
+	err = future.Error()
+	if err != nil {
+		log.Println("bootstrap cluster:", err)
 	}
 
 	return raftServer, nil
@@ -194,4 +221,21 @@ func (s snapshotNoop) Release() {}
 
 func newSnapshotNoop() (raft.FSMSnapshot, error) {
 	return &snapshotNoop{}, nil
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundTCPAddr() net.TCPAddr {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return net.TCPAddr{
+		IP:   localAddr.IP,
+		Port: localAddr.Port,
+		Zone: localAddr.Zone,
+	}
 }
